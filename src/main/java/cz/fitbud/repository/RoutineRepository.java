9139c675f2cf899af0bd8629d202062d000b8902
Matyáš Mayer <matyasmayer2004@gmail.com>
2026-08-03 20:27:01 +0200

    feat: Add repos for workout and routine

---
+++ src/main/java/cz/fitbud/repository/RoutineRepository.java
@@ -0,0 +1,9 @@
+package cz.fitbud.repository;
+
+import cz.fitbud.entity.Routine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoutineRepository extends JpaRepository<Routine,Long> {
+}
