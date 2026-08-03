b73c93eb91af8ec82cf4686752731b26c61f2028
Matyáš Mayer <matyasmayer2004@gmail.com>
2026-08-03 18:50:07 +0200

    feat: add functions for finding exercises by name and muscles to repo and add api for exercises

--- src/main/java/cz/fitbud/controller/ExerciseController.java
+++ src/main/java/cz/fitbud/controller/ExerciseController.java
@@ -4,8 +4,10 @@ import cz.fitbud.entity.Exercise;
 import cz.fitbud.repository.ExerciseRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/exercises")
@@ -20,6 +22,27 @@ public class ExerciseController {
     @GetMapping
     public List<Exercise> getExercises(){
         return exerciseRepo.findAll();
+    }
+
+    //for dropdown menu
+    @GetMapping("/muscles")
+    public List<String> getMuscles(){
+        return exerciseRepo.findAllDistinctPrimaryMuscles();
+    }
+
+    @GetMapping("/search")
+    public List<Exercise> searchExercises (@RequestParam(defaultValue = "") String name,@RequestParam(defaultValue = "") String muscle){
+
+        if(Objects.equals(name, "") && Objects.equals(muscle, "")){
+            return exerciseRepo.findAll();
+        }else if(!Objects.equals(name, "") && Objects.equals(muscle, "")){
+            return exerciseRepo.findByNameContainingIgnoreCase(name);
+        }else if (Objects.equals(name, "")){
+            return exerciseRepo.findByPrimaryMusclesContainingIgnoreCase(muscle);
+        }else{
+            return exerciseRepo.findByNameContainingIgnoreCaseAndPrimaryMusclesContainingIgnoreCase(name,muscle);
+        }
+
     }
 
 }
--- src/main/java/cz/fitbud/repository/ExerciseRepository.java
+++ src/main/java/cz/fitbud/repository/ExerciseRepository.java
@@ -2,8 +2,22 @@ package cz.fitbud.repository;
 
 import cz.fitbud.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
-public interface ExerciseRepository extends JpaRepository<Exercise,Long> { }
+public interface ExerciseRepository extends JpaRepository<Exercise,Long> {
+
+    List<Exercise> findByNameContainingIgnoreCase(String name);
+
+    List<Exercise> findByPrimaryMusclesContainingIgnoreCase(String muscle);
+
+    List<Exercise> findByNameContainingIgnoreCaseAndPrimaryMusclesContainingIgnoreCase (String name, String primaryMuscles);
+
+    // For dropdown menu
+    @Query("SELECT DISTINCT m FROM Exercise e JOIN e.primaryMuscles m")
+    List<String> findAllDistinctPrimaryMuscles();
+}
